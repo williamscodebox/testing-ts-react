1d4b03a72468d0a76fd723982b8b4b839ffb275c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-25 03:59:54 -0400

    added tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,2 +1,7 @@
 // vitest.setup.ts
 import "@testing-library/jest-dom"; // For better assertions
+import { server } from "./components/mocks/server.ts";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
