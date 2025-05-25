eadf831c4b7f61551e938647ff2c13b6b3b72a3a
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-25 16:53:43 -0400

    updated button story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -12,3 +12,8 @@ const danger = "bg-red-500 text-white font-bold py-2 px-4 rounded";
 export const Primary = () => <Button className={primary}>Primary</Button>;
 export const Secondary = () => <Button className={secondary}>Secondary</Button>;
 export const Danger = () => <Button className={danger}>Danger</Button>;
+
+// Renaming Stories
+Primary.storyName = "Blue Button";
+Secondary.storyName = "Cyan Button";
+Danger.storyName = "Red Button";
