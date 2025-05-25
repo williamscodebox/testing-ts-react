4d25ec644544667bedcd5dc3b91dfa7061eb41cf
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-25 16:52:00 -0400

    updated  storybook setup

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+import Button from "./Button";
+
+export default {
+  title: "products/buttons",
+  component: Button,
+};
+
+const primary = "bg-blue-500 text-white font-bold py-2 px-4 rounded";
+const secondary = "bg-cyan-500 text-white font-bold py-2 px-4 rounded";
+const danger = "bg-red-500 text-white font-bold py-2 px-4 rounded";
+
+export const Primary = () => <Button className={primary}>Primary</Button>;
+export const Secondary = () => <Button className={secondary}>Secondary</Button>;
+export const Danger = () => <Button className={danger}>Danger</Button>;
