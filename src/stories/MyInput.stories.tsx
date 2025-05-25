a3d0884b47dc5d6b491165efa8f37930679aab3d
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-25 17:46:57 -0400

    added stories

---
+++ src/stories/MyInput.stories.tsx
@@ -0,0 +1,8 @@
+import MyInput from "./MyInput";
+
+export default {
+  title: "inputs",
+  component: MyInput,
+};
+
+export const Primary = () => <MyInput className="text-2xl" />;
---
+++ src/stories/MyInput.tsx
@@ -0,0 +1,9 @@
+function MyInput(props: any) {
+  return (
+    <div {...props} className={props.className}>
+      Test
+    </div>
+  );
+}
+
+export default MyInput;
